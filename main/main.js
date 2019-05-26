adca2161773c09d5a90f710be4e50131e0bda656
BIGW3NDY <380769913@qq.com>
2019-05-26 14:47:41 +0800

    first commit

--- main/main.js
+++ main/main.js
@@ -1,4 +1,23 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var costOfDistance = calDistanceCost(inputs.distance);
+	var costOfParking = calParkingCost(inputs.parkTime);
+	console.log(costOfDistance);
+	console.log(costOfParking);
+	return Math.round(costOfDistance + costOfParking);
 };
+
+function calDistanceCost(distance){
+	var cost = 6;
+	if(distance <= 8 && distance > 2){
+		cost = cost + 0.8*(distance-2);
+	}else if(distance > 8){
+		cost += (8-2)*0.8;
+		cost = cost + 0.8*1.5*(distance-8);
+	}
+	return cost;
+}
+
+function calParkingCost(parkTime){
+	return parkTime*0.25;
+}
